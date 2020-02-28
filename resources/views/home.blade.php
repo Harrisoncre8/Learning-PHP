7811e0aa557cb3f268dbe959cd10419c40ecc365
Harrison Nguyen <nguy2173@umn.edu>
2020-02-28 14:46:05 -0600

    Add front end formating to home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,12 +5,32 @@
     <div class="row">
         <div class="col-3 p-5">
             <img src="https://www.pngkey.com/png/full/15-150980_book-svg-logo-png-book-icon-wikimedia.png" 
-                style="height: 200px;" class="rounded-circle" />
+                style="height: 150px;" class="rounded-circle" />
         </div>
-        <div class="col-9">
-            <div>
-                <h1>Learning PHP</h1>
+        <div class="col-9 pt-5">
+            <div> <h1>Learning PHP</h1> </div>
+            <div class="d-flex">
+                <div class="pr-5"> <strong>153</strong> Posts </div>
+                <div class="pr-5"> <strong>23k</strong> Followers </div>
+                <div class="pr-5"> <strong>212</strong> Following </div>
             </div>
+            <div class="pt-4 font-weight-bold">github.com/Harrisoncre8</div>
+            <div>Creating a small CRUD app to learn PHP.</div>
+            <div> <a href="http://">github.com/Harrisoncre8</a> </div>
+        </div>
+    </div>
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="https://cpb-ap-se2.wpmucdn.com/mediafactory.org.au/dist/f/164/files/2014/01/article-2256067-16B964D7000005DC-733_634x409-1fdvpet.jpg" 
+                 class="w-100 h-100"/>
+        </div>
+        <div class="col-4">
+            <img src="https://hylo.de/eye-care/_processed_/c/f/csm_pc-arbeit-trockene-augen_59627154d7.jpg" 
+                 class="w-100 h-100"/>
+        </div>
+        <div class="col-4">
+            <img src="https://img.etimg.com/thumb/msid-50496451,width-643,imgsize-285478,resizemode-4/do-you-sit-in-front-of-the-computer-all-day-tips-to-avoid-computer-vision-syndrome.jpg" 
+                 class="w-100 h-100"/>
         </div>
     </div>
 </div>
