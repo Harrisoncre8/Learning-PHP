37ca913e7fe5a1e009b316835539f17d10763e61
Harrison Nguyen <nguy2173@umn.edu>
2020-02-28 16:31:42 -0600

    Add username column to database

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,16 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
+        // creates a users table with the following blueprints
         Schema::create('users', function (Blueprint $table) {
+            // bigIncrements means that the id is increase by 1 with each new iteration
             $table->bigIncrements('id');
+            // name is a string
             $table->string('name');
+            // email is a string that is unique to each user
             $table->string('email')->unique();
+            // two users cannot have the same username, it is unique at DB level
+            $table->string('username')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
