9510d557e0301f62286f40aef4eab498b537002d
Harrison Nguyen <nguy2173@umn.edu>
2020-02-28 17:17:44 -0600

    Getting username data on profile page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
                 style="height: 150px;" class="rounded-circle" />
         </div>
         <div class="col-9 pt-5">
-            <div> <h1>Learning PHP</h1> </div>
+            <div> <h1>{{ $user->username }}</h1> </div>
             <div class="d-flex">
                 <div class="pr-5"> <strong>153</strong> Posts </div>
                 <div class="pr-5"> <strong>23k</strong> Followers </div>
