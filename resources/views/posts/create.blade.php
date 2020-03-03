b90706cd168bf3254696e9da69f2757b024a7a79
Harrison Nguyen <nguy2173@umn.edu>
2020-03-03 17:15:36 -0600

    Create html format for image upload page

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <form action="">
+        <div class="row">
+            <div class="col-8 offset-2">
+
+                <div class="row">
+                    <h2>Add New Post</h2>
+                </div>
+
+                <div class="form-group row">
+                    <!-- Input for users to type in caption for image -->               
+                    <label for="caption" class="col-md-4 col-form-label">Post Caption</label>
+                    <input id="caption" 
+                        type="text" 
+                        class="form-control @error('caption') is-invalid @enderror" 
+                        caption="caption" 
+                        value="{{ old('caption') }}" 
+                        required autocomplete="caption" autofocus>
+
+                    @error('caption')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+                
+                <!-- Input for users to upload image -->
+                <div class="row">
+                    <label for="image" class="col-md-4 col-form-label">Post Image</label>
+                    <input type="file" class="form-control-file" id="image" name="image">
+                    @error('image')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+
+                <!-- Button for users to upload image to profile -->
+                <div class="row pt-4">
+                    <button class="btn btn-primary">Add New Post</button>
+                </div>
+
+            </div>
+        </div>
+    </form>
+</div>
+@endsection
