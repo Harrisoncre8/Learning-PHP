80453469ef8dc08589adf87a4b1c531bd2007b81
Harrison Nguyen <nguy2173@umn.edu>
2020-03-03 16:47:32 -0600

    Add link feature to front end

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,10 @@
                 style="height: 150px;" class="rounded-circle" />
         </div>
         <div class="col-9 pt-5">
-            <div> <h1>{{ $user->username }}</h1> </div>
+            <div class="d-flex justify-content-between align-items-baseline"> 
+                <h1>{{ $user->username }}</h1> 
+                <a href="#">Add New Post</a>
+            </div>
             <div class="d-flex">
                 <div class="pr-5"> <strong>153</strong> Posts </div>
                 <div class="pr-5"> <strong>23k</strong> Followers </div>
