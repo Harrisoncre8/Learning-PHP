e2ebbfed46e2e6bd4759149cdeb0c492920629f6
Harrison Nguyen <nguy2173@umn.edu>
2020-03-04 18:15:10 -0600

    Adding manual authorization to post

--- app/Post.php
+++ app/Post.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    // Telling laravel to allow developer to manually guard post data
+    protected $guaded = [];
+
     public function user()
     {
         return $this->belongsTo(User::class);
