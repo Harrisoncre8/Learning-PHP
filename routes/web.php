b521b5506685654e6d04c1143d9b44685f8d36fe
Harrison Nguyen <nguy2173@umn.edu>
2020-03-11 14:51:35 -0500

    Add route to update profile changes

--- routes/web.php
+++ routes/web.php
@@ -28,5 +28,8 @@ Route::post('/p', 'PostsController@store');
 // calling the index method inside the home controller
 // profile route for user profiles
 Route::get('/profile/{user}', 'ProfileController@index')->name('profile.show');
-// adding edit route for user to edit only their profile
+// adding edit route for user to edit their profile based on their id
 Route::get('/profile/{user}/edit', 'ProfileController@edit')->name('profile.edit');
+// put/patch route to update user profile
+Route::patch('/profile/{user}/', 'ProfileController@edit')->name('profile.update');
+
