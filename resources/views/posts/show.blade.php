b4e9f1c22e90f2d2accf3254cb706823dd4a13f7
Harrison Nguyen <nguy2173@umn.edu>
2020-03-10 22:46:59 -0500

    Add caption and username to each post to show

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,6 +6,12 @@
       <div class="col-8">
         <img src="/storage/{{ $post->image }}" class="w-100">
       </div>
+      <div class="col-4">
+        <div>
+          <h3>{{ $post->user->username }}</h3>
+          <p>{{ $post->caption }}</p>
+        </div>
+      </div>
     </div>
 </div>
 @endsection
