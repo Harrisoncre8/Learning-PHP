d7cde9ba70feedd60aea0c75d6656d4014260b35
Harrison Nguyen <nguy2173@umn.edu>
2020-03-17 22:34:18 -0500

    Manually disabling protection on profile model

--- app/Profile.php
+++ app/Profile.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
+    // disabling protection
+    protected $guarded = [];
     public function user()
     {
         return $this->belongsTo(user::class);
