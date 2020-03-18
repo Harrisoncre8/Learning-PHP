4d515a064053f0e938edbeda6610c0f8c8757047
Harrison Nguyen <nguy2173@umn.edu>
2020-03-17 22:36:21 -0500

    Authorize profile policy to profile controller

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -22,19 +22,28 @@ class ProfileController extends Controller
     // this is another way of passing in user like we did above to the index public function
     public function edit(User $user)
     {
+        // authorizing the policy so that only users can update their profile
+        $this->authorize('update', $user->profile);
+
         return view('profiles.edit', compact('user'));
     }
     
     // form validation when updating profile changes
-    public function update()
+    public function update(User $user)
     {
+
+        $this->authorize('update', $user->profile);
+
         $data = request()->validate([
-            'title' => '',
-            'description' => '',
-            'url' => '',
+            'title' => 'required',
+            'description' => 'required',
+            'url' => 'url',
             'image' => '',
         ]);
+        
+        // a layer of protection where only authenticated users can edit their own profile
+        auth()->user()->profile->update($data);
 
-        dd($data);
+        return redirect("/profile/{$user->id}");
     }
 }
