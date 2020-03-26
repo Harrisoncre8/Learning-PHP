dc234e768a972f9e257e3cfd7bcdb92c2fbc3105
Harrison Nguyen <nguy2173@umn.edu>
2020-03-25 20:17:58 -0500

    Add uploaded or default profile image

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row">
         <div class="col-3 p-5">
-            <img src="/storage/{{ $user->profile->image }}" 
+            <img src="{{ $user->profile->profileImage() }}" 
                  class="rounded-circle w-100" />
         </div>
         <div class="col-9 pt-5">
