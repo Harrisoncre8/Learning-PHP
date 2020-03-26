de8594a5585f17fd0fe6e4cf70030d8422b9b7b7
Harrison Nguyen <nguy2173@umn.edu>
2020-03-25 20:18:20 -0500

    Add default image if no profile image is uploaded

--- app/Profile.php
+++ app/Profile.php
@@ -8,6 +8,14 @@ class Profile extends Model
 {
     // disabling protection
     protected $guarded = [];
+
+    // provide default image unless user uploads custom profile image
+    public function profileImage()
+    {
+        $imagePath = ($this->image) ? $this->image : '/storage/profile/NOQAzHUcsbR8RBsnHXMLewP0eQBqobCb5ueoDR6H.png';
+        return '/storage/' . $imagePath;
+    }
+
     public function user()
     {
         return $this->belongsTo(user::class);
