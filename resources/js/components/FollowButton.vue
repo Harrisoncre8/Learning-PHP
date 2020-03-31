64a301e9be4488080f9510f202e40df63ef54e04
Harrison Nguyen <nguy2173@umn.edu>
2020-03-30 19:45:50 -0500

    Add vue method to make server request to follow

--- resources/js/components/FollowButton.vue
+++ resources/js/components/FollowButton.vue
@@ -6,13 +6,18 @@
 
 <script>
     export default {
+        props: ['userId'],
+
         mounted() {
             console.log('Component mounted.')
         },
 
         methods: {
             followUser() {
-                alert('inside');
+                axios.post('/follow/' + this.userId)
+                    .then(response => {
+                        alert(response.data);
+                    });
             }
         }
     }
